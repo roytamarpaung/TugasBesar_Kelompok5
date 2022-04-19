7a1eaea6ad76272af711eefa3338d924b56984d2
roytamarpaung <100549964+roytamarpaung@users.noreply.github.com>
2022-04-19 21:23:39 +0700

    Membuat Profil

---
+++ Profil.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace _1302204061
+{
+    partial class Profil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(288, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MY PROFILE";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(294, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Edit Profil";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(294, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Ubah Password";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(294, 286);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 47);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Ubah Foto Profil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Profil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Profil";
+            this.Text = "Profil";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Profil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1302204061
+{
+    public partial class Profil : Form
+    {
+        public Profil()
+        {
+            InitializeComponent();
+            //Title Window
+            Text = "Profil";
+            // ukuran window
+            Size = new Size(800, 600);
+            // Posisi Window berada di tengah layar
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //button untuk edit profile
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //button untuk mengubah password
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //button untuk mengubah foto profil
+        }
+    }
+}
