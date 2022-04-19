4f5c7427901bf0d5065c5ebd63045e2b96214d6b
roytamarpaung <100549964+roytamarpaung@users.noreply.github.com>
2022-04-19 21:28:09 +0700

    first init

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace _1302204061
 {
-    internal class Program
+
+    static class Program
     {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //penguna
+            //run the application
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+            Application.Run(new CreateAccount());
+            Application.Run(new UbahPassword());
+            Application.Run(new Keuangan());
+            Application.Run(new MasukkanSaldo());
+            Application.Run(new AmbilSaldo());
+
+
+        }
     }
 }
