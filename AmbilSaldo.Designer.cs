42298235d7fa53d89960c319480d17a63c44fa96
roytamarpaung <100549964+roytamarpaung@users.noreply.github.com>
2022-04-19 21:18:26 +0700

    Membuat Kelas dan Design Pengambilan Saldo

---
+++ AmbilSaldo.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace _1302204061
+{
+    
+    partial class AmbilSaldo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbAmbilSaldo = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textboxJumlah = new System.Windows.Forms.TextBox();
+            this.textboxTanggal = new System.Windows.Forms.TextBox();
+            this.lbJumlah = new System.Windows.Forms.Label();
+            this.lbTanggal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbAmbilSaldo
+            // 
+            this.lbAmbilSaldo.AutoSize = true;
+            this.lbAmbilSaldo.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAmbilSaldo.Location = new System.Drawing.Point(312, 62);
+            this.lbAmbilSaldo.Name = "lbAmbilSaldo";
+            this.lbAmbilSaldo.Size = new System.Drawing.Size(203, 30);
+            this.lbAmbilSaldo.TabIndex = 1;
+            this.lbAmbilSaldo.Text = "Form Ambil Saldo";
+            this.lbAmbilSaldo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(659, 293);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 35);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "Close";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(499, 293);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 35);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Cetak Bukti";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(316, 293);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 35);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Blank Form";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(181, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 35);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Hapus";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(58, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Simpan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textboxJumlah
+            // 
+            this.textboxJumlah.Location = new System.Drawing.Point(316, 164);
+            this.textboxJumlah.Name = "textboxJumlah";
+            this.textboxJumlah.Size = new System.Drawing.Size(323, 26);
+            this.textboxJumlah.TabIndex = 14;
+            this.textboxJumlah.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textboxTanggal
+            // 
+            this.textboxTanggal.Location = new System.Drawing.Point(316, 122);
+            this.textboxTanggal.Name = "textboxTanggal";
+            this.textboxTanggal.Size = new System.Drawing.Size(323, 26);
+            this.textboxTanggal.TabIndex = 13;
+            this.textboxTanggal.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lbJumlah
+            // 
+            this.lbJumlah.AutoSize = true;
+            this.lbJumlah.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbJumlah.Location = new System.Drawing.Point(106, 167);
+            this.lbJumlah.Name = "lbJumlah";
+            this.lbJumlah.Size = new System.Drawing.Size(60, 21);
+            this.lbJumlah.TabIndex = 12;
+            this.lbJumlah.Text = "Jumlah";
+            this.lbJumlah.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lbTanggal
+            // 
+            this.lbTanggal.AutoSize = true;
+            this.lbTanggal.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTanggal.Location = new System.Drawing.Point(106, 129);
+            this.lbTanggal.Name = "lbTanggal";
+            this.lbTanggal.Size = new System.Drawing.Size(140, 21);
+            this.lbTanggal.TabIndex = 11;
+            this.lbTanggal.Text = "Masukkan Tanggal";
+            this.lbTanggal.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // AmbilSaldo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textboxJumlah);
+            this.Controls.Add(this.textboxTanggal);
+            this.Controls.Add(this.lbJumlah);
+            this.Controls.Add(this.lbTanggal);
+            this.Controls.Add(this.lbAmbilSaldo);
+            this.Name = "AmbilSaldo";
+            this.Text = "AmbilSaldo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbAmbilSaldo;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textboxJumlah;
+        private System.Windows.Forms.TextBox textboxTanggal;
+        private System.Windows.Forms.Label lbJumlah;
+        private System.Windows.Forms.Label lbTanggal;
+    }
+}
---
+++ AmbilSaldo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1302204061
+{
+    public class PengeluaranSaldo
+    {
+        public enum Bulan
+        {
+            Januari, Februari, Maret, April, Mei, Juni, Juli, Agustus, September,
+            Oktober, November, Desember
+        }
+        public static string GetPengeluaran(Bulan month)
+        {
+            string[] PengeluaranPerMonth = { "1000000", "2000000", "3000000", "4000000", "5000000", "6000000", "7000000", "8000000", "9000000", "10000000", "11000000", "12000000" };
+            return PengeluaranPerMonth[(int)month];
+        }
+    }
+
+    public partial class AmbilSaldo : Form
+    {
+        public AmbilSaldo()
+         {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            lbAmbilSaldo.Text = " ";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            lbTanggal.Text = " ";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            lbJumlah.Text = " ";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textboxTanggal.Text = "";
+            textboxJumlah.Text = "";
+            MessageBox.Show(" Berhasil Menyimpan ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textboxTanggal.Text = "";
+            textboxJumlah.Text = "";
+            MessageBox.Show(" Berhasil Menghapus ");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textboxTanggal.Text = "";
+            textboxJumlah.Text = "";
+            MessageBox.Show(" Blank Form ? ");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textboxTanggal.Text = "";
+            textboxJumlah.Text = "";
+            MessageBox.Show("Berhasil Mencetak Bukti");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textboxTanggal.Text = "";
+            textboxJumlah.Text = "";
+            MessageBox.Show(" Menutup Program ");
+        }
+    }
+}
