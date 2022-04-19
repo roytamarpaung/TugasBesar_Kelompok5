e6f20f85fa11276a2d1fe7aa1038d38ca8ef9578
roytamarpaung <100549964+roytamarpaung@users.noreply.github.com>
2022-04-19 21:36:17 +0700

    Melakukan Teknik Table Driven

---
+++ Pengguna.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1302204061
+{
+    public class Pengguna
+    {
+        public enum DataPengguna
+        {
+            NamaLengkap,
+            NIK,
+            TempatLahir,
+            TanggalLahir,
+            Phone,
+            Email,
+            Username,
+            Password
+        }
+        public static String GetPengguna(DataPengguna pengguna)
+        {
+            string[] DataPengguna = { " Nama Lengkap ", " NIK ", " Tempat Lahir ", " Tanggal Lahir ", " Phone ", " Email ", " Username ", " Password " };
+            return DataPengguna[(int)pengguna];
+        }
+    }
+
+}
