0b41fd22995edbd5e07603275499149e58393870
roytamarpaung <100549964+roytamarpaung@users.noreply.github.com>
2022-04-19 21:22:24 +0700

    Membuat class dan design masukkan saldo

---
+++ MasukkanSaldo.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace _1302204061
+{
+    partial class MasukkanSaldo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMasukSaldo = new System.Windows.Forms.Label();
+            this.lbTanggal = new System.Windows.Forms.Label();
+            this.lbJumlah = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnBlankForm = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbMasukSaldo
+            // 
+            this.lbMasukSaldo.AutoSize = true;
+            this.lbMasukSaldo.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMasukSaldo.Location = new System.Drawing.Point(317, 54);
+            this.lbMasukSaldo.Name = "lbMasukSaldo";
+            this.lbMasukSaldo.Size = new System.Drawing.Size(262, 30);
+            this.lbMasukSaldo.TabIndex = 0;
+            this.lbMasukSaldo.Text = "Form Pemasukan Saldo";
+            this.lbMasukSaldo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbTanggal
+            // 
+            this.lbTanggal.AutoSize = true;
+            this.lbTanggal.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTanggal.Location = new System.Drawing.Point(111, 148);
+            this.lbTanggal.Name = "lbTanggal";
+            this.lbTanggal.Size = new System.Drawing.Size(140, 21);
+            this.lbTanggal.TabIndex = 1;
+            this.lbTanggal.Text = "Masukkan Tanggal";
+            this.lbTanggal.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lbJumlah
+            // 
+            this.lbJumlah.AutoSize = true;
+            this.lbJumlah.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbJumlah.Location = new System.Drawing.Point(111, 186);
+            this.lbJumlah.Name = "lbJumlah";
+            this.lbJumlah.Size = new System.Drawing.Size(60, 21);
+            this.lbJumlah.TabIndex = 2;
+            this.lbJumlah.Text = "Jumlah";
+            this.lbJumlah.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(111, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(321, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(323, 26);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(321, 183);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(323, 26);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSimpan.Location = new System.Drawing.Point(186, 312);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(84, 35);
+            this.btnSimpan.TabIndex = 6;
+            this.btnSimpan.Text = " Simpan ";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHapus.Location = new System.Drawing.Point(186, 312);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(84, 35);
+            this.btnHapus.TabIndex = 7;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnBlankForm
+            // 
+            this.btnBlankForm.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBlankForm.Location = new System.Drawing.Point(321, 312);
+            this.btnBlankForm.Name = "btnBlankForm";
+            this.btnBlankForm.Size = new System.Drawing.Size(127, 35);
+            this.btnBlankForm.TabIndex = 8;
+            this.btnBlankForm.Text = "Blank Form";
+            this.btnBlankForm.UseVisualStyleBackColor = true;
+            this.btnBlankForm.Click += new System.EventHandler(this.button3_Click);
+            //  
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(664, 312);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(83, 35);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // MasukkanSaldo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 403);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnBlankForm);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbJumlah);
+            this.Controls.Add(this.lbTanggal);
+            this.Controls.Add(this.lbMasukSaldo);
+            this.Name = "MasukkanSaldo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MasukkanSaldo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbMasukSaldo;
+        private System.Windows.Forms.Label lbTanggal;
+        private System.Windows.Forms.Label lbJumlah;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnBlankForm;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ MasukkanSaldo.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1302204061
+{
+    public class PemasukanSaldo
+    {
+        public enum Month
+        {
+            Januari, Februari, Maret, April, Mei, Juni, Juli, Agustus, September,
+            Oktober, November, Desember
+        }
+        public static String GetPemasukan(Month bulan)
+        {
+            string[] PemasukanPerMonth = { "1000000", "2000000", "3000000", "4000000", "5000000", "6000000", "7000000", "8000000", "9000000", "10000000", "11000000", "12000000" };
+            return PemasukanPerMonth[(int)bulan];
+        }
+    }
+
+    public partial class MasukkanSaldo : Form
+    {
+       
+        public MasukkanSaldo()
+        {
+            InitializeComponent();
+            //Title Window
+            Text = "Masukkan Saldo";
+            // ukuran window
+            Size = new Size(800, 600);
+            // Posisi Window berada di tengah layar
+            StartPosition = FormStartPosition.CenterScreen;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Masukkan Tanggal Inputan
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //Masukkan Jumlah yang Ingin dimasukkan
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = (" Masukkan Tanggal ");
+            textBox1.Text = (" Jumlah ");
+            MessageBox.Show(" Berhasil Menyimpan ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = (" Masukkan Tanggal ");
+            textBox1.Text = (" Jumlah ");
+            MessageBox.Show(" Berhasil Menghapus ");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = (" Masukkan Tanggal ");
+            textBox1.Text = (" Jumlah ");
+            MessageBox.Show(" Blank Form ? ");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+            textBox1.Text = (" Masukkan Tanggal ");
+            textBox1.Text = (" Jumlah ");
+            MessageBox.Show(" Menutup Program ");
+        }
+    }
+}
