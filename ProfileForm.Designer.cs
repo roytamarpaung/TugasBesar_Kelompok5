9aef0d67cea55b39597405d8264f408db729c58b
putrilarasxx@365.telkomuniversity.ac.id <putrilarasati440@gmail.com>
2022-04-19 22:18:38 +0700

    menambahkan class ProfileForm - Runtime Configuration

---
+++ ProfileForm.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace APL
+{
+    partial class ProfileForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(128, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(538, 335);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(356, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(356, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(356, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(356, 215);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(96, 250);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(356, 265);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "label10";
+            // 
+            // ProfileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ProfileForm";
+            this.Text = "ProfileForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ProfileForm.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace APL
+{
+    public partial class ProfileForm : Form
+    {
+        public ProfileForm()
+        {
+            InitializeComponent();
+            Text = "Menu Profile";
+            Size = new Size(800, 500);
+            StartPosition = FormStartPosition.CenterScreen;
+            MenuProfileConfig objek = new MenuProfileConfig();
+            label5.Text = objek.conf.NamaDepan;
+            label6.Text = objek.conf.NamaBelakang;
+            label7.Text = objek.conf.Username;
+            label8.Text = objek.conf.Email;
+            label10.Text = objek.conf.Password;
+        }
+
+        //Membuat class untuk membaca dan menulis file konfigurasi
+        class MenuProfileConfig
+        {
+            public ProfileConfig conf;
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
+            public string fileConfigName = "profile.json";
+            public MenuProfileConfig()
+            {
+                try
+                {
+                    ReadConfigFile();
+                }
+                catch
+                {
+                    SetDefault();
+                    WriteConfigFile();
+                }
+            }
+            private void SetDefault()
+            {
+                string objekNamaDepan = "Marin";
+
+                string objekNamaBelakang = "Kitagawa";
+
+                string objekUsername = "marinktgw";
+
+                string objekEmail = "marin@gmail.com";
+
+                string objekPassword = "mari123";
+
+                conf = new ProfileConfig(objekNamaDepan, objekNamaBelakang, objekUsername, objekEmail, objekPassword);
+            }
+            //Membuat method untuk membaca file konfigurasi
+            public ProfileConfig ReadConfigFile()
+            {
+                string jsonStringFromFile = File.ReadAllText(path + "/" + fileConfigName);
+                conf = JsonConvert.DeserializeObject<ProfileConfig>(jsonStringFromFile);
+                return conf;
+            }
+            //Membuat method untuk menulis file konfigurasi
+            public void WriteConfigFile()
+            {
+                string jsonString = JsonConvert.SerializeObject(conf, Formatting.Indented);
+                string fullFilePath = path + "/" + fileConfigName;
+                File.WriteAllText(fullFilePath, jsonString);
+            }
+        }
+        //Membuat class penampung konfigurasi
+        class ProfileConfig
+        {
+            public string NamaDepan { get; set; }
+            public string NamaBelakang { get; set; }
+            public string Username { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public ProfileConfig(string namadepan, string namabelakang, string username, string email, string password)
+            {
+                NamaDepan = namadepan;
+                NamaBelakang = namabelakang;
+                Username = username;
+                Email = email;
+                Password = password;
+            }
+        }
+    }
+}
