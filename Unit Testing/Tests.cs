a71db7c71c63dd5ad13390b4f3d7348d9b1f91ca
realizyy <realizy@student.telkomuniversity.ac.id>
2022-07-02 07:09:00 +0700

    Menambahkan Unit Testing Untuk Library Count

---
+++ Unit Testing/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Unit_Testing
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Pertambahan()
+        {
+            int a = 200;
+            int b = 100;
+            int c = Count.Math.pertambahan(a, b);
+            
+            Assert.AreEqual(300, c);
+        }
+        
+        [Test]
+        public void Pengurangan()
+        {   
+            int a = 4000;
+            int b = 3000;
+            int c = Count.Math.pengurangan(a, b);
+            
+            Assert.AreEqual(1000, c);
+        }
+    }
+}
